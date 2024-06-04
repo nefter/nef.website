1cda1676aae2c458f10c2932d3c87d273c37c358
Nef Rossano <nefter@protonmail.com>
2024-06-04 12:36:07 -0600

    Update logo Image

--- components/Header.js
+++ components/Header.js
@@ -1,9 +1,11 @@
 import Link from 'next/link';
+import Image from 'next/image';
+import logoImage from '../public/images/logoImage.png';
 
 export default function Header({ name }) {
   return (
     <header className="pt-20 pb-12">
-      <div className="w-12 h-12 rounded-full block mx-auto mb-4 bg-gradient-conic from-gradient-3 to-gradient-4" />
+      <Image src={logoImage} alt="Logo Nef" className="w-12 h-12 rounded-full block mx-auto mb-4"/>
       <p className="text-2xl dark:text-white text-center">
         <Link href="/">
           <a>{name}</a>
